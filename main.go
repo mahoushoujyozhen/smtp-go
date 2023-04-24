5597302363c2d9b54fd7f99d89e81fe062a31306
HuangZhen <389403710@qq.com>
2023-04-24 22:44:06 +0800

    implement Short Message Service(SMS) with Ali Cloud

--- main.go
+++ main.go
@@ -1,29 +1,39 @@
 package main
 
 import (
-	"email/smtpEmail"
+	"email/shortMsg"
 	"fmt"
+	"github.com/alibabacloud-go/tea/tea"
+	"os"
 )
 
 func main() {
 	fmt.Println("main")
-	//smtpEmail.SendEmail()
-	receivers := []string{"389403710@qq.com"}
-	subject := "我是主题"
-	body := "你好呀，很高兴见到你，我是body"
-	attach := "./hello.txt"
-	msg := &smtpEmail.Message{
-		Subject: subject,
-		Body:    body,
-		Attach:  attach,
+	////smtpEmail.SendEmail()
+	//receivers := []string{"389403710@qq.com"}
+	//subject := "我是主题"
+	//body := "你好呀，很高兴见到你，我是body"
+	//attach := "./hello.txt"
+	//msg := &smtpEmail.Message{
+	//	Subject: subject,
+	//	Body:    body,
+	//	Attach:  attach,
+	//}
+	//emailInfo := &smtpEmail.Email{
+	//	SMTPHost: "smtp.qq.com",
+	//	SMTPPort: 587,
+	//	From:     "389403710@qq.com",
+	//	Password: "sybagzuwgjstbjgi",
+	//	To:       receivers,
+	//	Msg:      msg,
+	//}
+	//smtpEmail.SendEmail(emailInfo)
+
+	err := shortMsg.Send(tea.StringSlice(os.Args[1:]))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
 	}
-	emailInfo := &smtpEmail.Email{
-		SMTPHost: "smtp.qq.com",
-		SMTPPort: 587,
-		From:     "389403710@qq.com",
-		Password: "12345",
-		To:       receivers,
-		Msg:      msg,
-	}
-	smtpEmail.SendEmail(emailInfo)
+	fmt.Println("success")
+
 }
---
+++ shortMsg/shortmsg.go
@@ -0,0 +1,109 @@
+package shortMsg
+
+import (
+	"fmt"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v3/client"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"math/rand"
+	"time"
+)
+
+/**
+ * 使用AK&SK初始化账号Client
+ * @param accessKeyId
+ * @param accessKeySecret
+ * @return Client
+ * @throws Exception
+ */
+
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *dysmsapi20170525.Client, _err error) {
+	config := &openapi.Config{
+		// 必填，您的 AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 必填，您的 AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
+	_result = &dysmsapi20170525.Client{}
+	_result, _err = dysmsapi20170525.NewClient(config)
+	return _result, _err
+}
+
+func CreateClientWithSTS(accessKeyId *string, accessKeySecret *string, securityToken *string) (_result *dysmsapi20170525.Client, _err error) {
+	config := &openapi.Config{
+		// 必填，您的 AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 必填，您的 AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+		// 必填，您的 Security Token
+		SecurityToken: securityToken,
+		// 必填，表明使用 STS 方式
+		Type: tea.String("sts"),
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
+	_result = &dysmsapi20170525.Client{}
+	_result, _err = dysmsapi20170525.NewClient(config)
+	return _result, _err
+}
+
+// 生成区间段内的随机数
+
+func RanInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	if min >= max || min == 0 || max == 0 {
+		return max
+	}
+	return rand.Intn(max-min+1) + min
+}
+
+func Send(args []*string) (_err error) {
+	// 工程代码泄露可能会导致AccessKey泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378661.html
+	client, _err := CreateClient(tea.String("accessKeyId"), tea.String("accessKeySecret"))
+	if _err != nil {
+		return _err
+	}
+
+	num := RanInt(1000, 9999)
+	fmt.Println(num, "******")
+	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+		SignName:     tea.String("mahoushoujyo"),
+		TemplateCode: tea.String("SMS_276312141"),
+		PhoneNumbers: tea.String("18406666122"),
+		//TemplateParam: tea.String("{\"code\":\"8888\"}"),
+		TemplateParam: tea.String(fmt.Sprintf("{\"code\":\"%d\"}", num)),
+	}
+	runtime := &util.RuntimeOptions{}
+	tryErr := func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		// 复制代码运行请自行打印 API 的返回值
+		_, _err = client.SendSmsWithOptions(sendSmsRequest, runtime)
+		if _err != nil {
+			return _err
+		}
+
+		return nil
+	}()
+
+	if tryErr != nil {
+		var error = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			error = _t
+		} else {
+			error.Message = tea.String(tryErr.Error())
+		}
+		// 如有需要，请打印 error
+		_, _err = util.AssertAsString(error.Message)
+		if _err != nil {
+			return _err
+		}
+	}
+	return _err
+}
